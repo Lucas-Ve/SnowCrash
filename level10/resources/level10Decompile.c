1439ea45afcdd957b0beba2f248cf1fc05b983ce
Lucas-Ve <lucasveloso@sfr.fr>
2025-09-09 16:11:51 +0200

    level10 decompileur

---
+++ level10/resources/level10Decompile.c
@@ -0,0 +1,425 @@
+#include "out.h"
+
+
+
+int _init(EVP_PKEY_CTX *ctx)
+
+{
+  int iVar1;
+  
+  __gmon_start__();
+  frame_dummy();
+  iVar1 = __do_global_ctors_aux();
+  return iVar1;
+}
+
+
+
+void FUN_08048500(void)
+
+{
+  (*(code *)(undefined *)0x0)();
+  return;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+ssize_t read(int __fd,void *__buf,size_t __nbytes)
+
+{
+  ssize_t sVar1;
+  
+  sVar1 = read(__fd,__buf,__nbytes);
+  return sVar1;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+int printf(char *__format,...)
+
+{
+  int iVar1;
+  
+  iVar1 = printf(__format);
+  return iVar1;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+int fflush(FILE *__stream)
+
+{
+  int iVar1;
+  
+  iVar1 = fflush(__stream);
+  return iVar1;
+}
+
+
+
+void __stack_chk_fail(void)
+
+{
+                    // WARNING: Subroutine does not return
+  __stack_chk_fail();
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+uint16_t htons(uint16_t __hostshort)
+
+{
+  uint16_t uVar1;
+  
+  uVar1 = htons(__hostshort);
+  return uVar1;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+int puts(char *__s)
+
+{
+  int iVar1;
+  
+  iVar1 = puts(__s);
+  return iVar1;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+char * strerror(int __errnum)
+
+{
+  char *pcVar1;
+  
+  pcVar1 = strerror(__errnum);
+  return pcVar1;
+}
+
+
+
+void __gmon_start__(void)
+
+{
+  __gmon_start__();
+  return;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+void exit(int __status)
+
+{
+                    // WARNING: Subroutine does not return
+  exit(__status);
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+int open(char *__file,int __oflag,...)
+
+{
+  int iVar1;
+  
+  iVar1 = open(__file,__oflag);
+  return iVar1;
+}
+
+
+
+void __libc_start_main(void)
+
+{
+  __libc_start_main();
+  return;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+ssize_t write(int __fd,void *__buf,size_t __n)
+
+{
+  ssize_t sVar1;
+  
+  sVar1 = write(__fd,__buf,__n);
+  return sVar1;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+int * __errno_location(void)
+
+{
+  int *piVar1;
+  
+  piVar1 = __errno_location();
+  return piVar1;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+int access(char *__name,int __type)
+
+{
+  int iVar1;
+  
+  iVar1 = access(__name,__type);
+  return iVar1;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+int socket(int __domain,int __type,int __protocol)
+
+{
+  int iVar1;
+  
+  iVar1 = socket(__domain,__type,__protocol);
+  return iVar1;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+in_addr_t inet_addr(char *__cp)
+
+{
+  in_addr_t iVar1;
+  
+  iVar1 = inet_addr(__cp);
+  return iVar1;
+}
+
+
+
+// WARNING: Unknown calling convention -- yet parameter storage is locked
+
+int connect(int __fd,sockaddr *__addr,socklen_t __len)
+
+{
+  int iVar1;
+  
+  iVar1 = connect(__fd,__addr,__len);
+  return iVar1;
+}
+
+
+
+void processEntry _start(undefined4 param_1,undefined4 param_2)
+
+{
+  undefined1 auStack_4 [4];
+  
+  __libc_start_main(main,param_2,&stack0x00000004,__libc_csu_init,__libc_csu_fini,param_1,auStack_4)
+  ;
+  do {
+                    // WARNING: Do nothing block with infinite loop
+  } while( true );
+}
+
+
+
+// WARNING: Removing unreachable block (ram,0x0804867a)
+// WARNING: Removing unreachable block (ram,0x08048680)
+
+void __do_global_dtors_aux(void)
+
+{
+  if (completed_6159 == '\0') {
+    completed_6159 = '\x01';
+  }
+  return;
+}
+
+
+
+// WARNING: Removing unreachable block (ram,0x080486bf)
+// WARNING: Removing unreachable block (ram,0x080486c8)
+
+void frame_dummy(void)
+
+{
+  return;
+}
+
+
+
+// WARNING: Unknown calling convention
+
+int main(int argc,char **argv)
+
+{
+  char *__cp;
+  uint16_t uVar1;
+  int iVar2;
+  int iVar3;
+  ssize_t sVar4;
+  size_t __n;
+  int *piVar5;
+  char *pcVar6;
+  int in_GS_OFFSET;
+  undefined4 *in_stack_00000008;
+  char *file;
+  char *host;
+  int fd;
+  int ffd;
+  int rc;
+  char buffer [4096];
+  sockaddr_in sin;
+  undefined1 local_1024 [4096];
+  sockaddr local_24;
+  int local_14;
+  
+  local_14 = *(int *)(in_GS_OFFSET + 0x14);
+  if (argc < 3) {
+    printf("%s file host\n\tsends file to host if you have access to it\n",*in_stack_00000008);
+                    // WARNING: Subroutine does not return
+    exit(1);
+  }
+  pcVar6 = (char *)in_stack_00000008[1];
+  __cp = (char *)in_stack_00000008[2];
+  iVar2 = access((char *)in_stack_00000008[1],4);
+  if (iVar2 == 0) {
+    printf("Connecting to %s:6969 .. ",__cp);
+    fflush(stdout);
+    iVar2 = socket(2,1,0);
+    local_24.sa_data[2] = '\0';
+    local_24.sa_data[3] = '\0';
+    local_24.sa_data[4] = '\0';
+    local_24.sa_data[5] = '\0';
+    local_24.sa_data[6] = '\0';
+    local_24.sa_data[7] = '\0';
+    local_24.sa_data[8] = '\0';
+    local_24.sa_data[9] = '\0';
+    local_24.sa_data[10] = '\0';
+    local_24.sa_data[0xb] = '\0';
+    local_24.sa_data[0xc] = '\0';
+    local_24.sa_data[0xd] = '\0';
+    local_24.sa_family = 2;
+    local_24.sa_data[0] = '\0';
+    local_24.sa_data[1] = '\0';
+    local_24.sa_data._2_4_ = inet_addr(__cp);
+    uVar1 = htons(0x1b39);
+    local_24.sa_data._0_2_ = uVar1;
+    iVar3 = connect(iVar2,&local_24,0x10);
+    if (iVar3 == -1) {
+      printf("Unable to connect to host %s\n",__cp);
+                    // WARNING: Subroutine does not return
+      exit(1);
+    }
+    sVar4 = write(iVar2,".*( )*.\n",8);
+    if (sVar4 == -1) {
+      printf("Unable to write banner to host %s\n",__cp);
+                    // WARNING: Subroutine does not return
+      exit(1);
+    }
+    printf("Connected!\nSending file .. ");
+    fflush(stdout);
+    iVar3 = open(pcVar6,0);
+    if (iVar3 == -1) {
+      puts("Damn. Unable to open file");
+                    // WARNING: Subroutine does not return
+      exit(1);
+    }
+    __n = read(iVar3,local_1024,0x1000);
+    if (__n == 0xffffffff) {
+      piVar5 = __errno_location();
+      pcVar6 = strerror(*piVar5);
+      printf("Unable to read from file: %s\n",pcVar6);
+                    // WARNING: Subroutine does not return
+      exit(1);
+    }
+    write(iVar2,local_1024,__n);
+    iVar2 = puts("wrote file!");
+  }
+  else {
+    iVar2 = printf("You don\'t have access to %s\n",pcVar6);
+  }
+  if (local_14 != *(int *)(in_GS_OFFSET + 0x14)) {
+                    // WARNING: Subroutine does not return
+    __stack_chk_fail();
+  }
+  return iVar2;
+}
+
+
+
+// WARNING: Function: __i686.get_pc_thunk.bx replaced with injection: get_pc_thunk_bx
+// WARNING: Removing unreachable block (ram,0x080489a0)
+// WARNING: Removing unreachable block (ram,0x080489a8)
+
+void __libc_csu_init(void)
+
+{
+  EVP_PKEY_CTX *in_stack_ffffffd4;
+  
+  _init(in_stack_ffffffd4);
+  return;
+}
+
+
+
+void __libc_csu_fini(void)
+
+{
+  return;
+}
+
+
+
+// WARNING: This is an inlined function
+
+void __i686_get_pc_thunk_bx(void)
+
+{
+  return;
+}
+
+
+
+// WARNING: Removing unreachable block (ram,0x08048a01)
+// WARNING: Removing unreachable block (ram,0x08048a08)
+
+void __do_global_ctors_aux(void)
+
+{
+  return;
+}
+
+
+
+void _fini(void)
+
+{
+  __do_global_dtors_aux();
+  return;
+}
+
+
+
